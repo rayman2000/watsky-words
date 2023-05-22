fa92dd62dd025f892d0db357685bf8e8db073061
Nicolas Klose <nicolas.klose@inf.ethz.ch>
2023-05-22 14:44:47 +0200

    Generic numbers

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ INT ENT ION
 
 This raises the question: what other such words can we find?
 
+# Usage
+Build using ```ghc --make watsky``` and run with ```./watsky sylLength numWords fileName```, where ```sylLength``` is the number of letter per matching syllable, ```numWords``` is the number of words to find, and ```fileName``` is the name of a file which contains a list of words, one per line. The example above can be generated with ```./watsky 3 3 words.txt```.
--- watsky.hs
+++ watsky.hs
@@ -1,31 +1,43 @@
-import Control.Arrow (Arrow (second))
-
+import System.Environment
 main = do
-  words <- readFile "words.txt"
-  print (watskyWords (lines words))
+  args <- getArgs  
+  let sylLength = read (head args) :: Int
+  let numWords = read (args !! 1) :: Int
+  let fileName = args !! 2
+  words <- readFile fileName
+  print (generalWatskyWords sylLength numWords (lines words))
 
-getSubstring :: String -> Int -> Int -> String
-getSubstring str start end = take (end - start) (drop start str)
+generalWatskyWords :: Int -> Int -> [String] -> [[String]]
+generalWatskyWords sylLength numWords words =
+  let lengthWords = filter (\s -> length s == sylLength * numWords) words
+   in extend lengthWords sylLength numWords [[word] | word <- lengthWords]
 
-watskyWords :: [String] -> [[String]]
-watskyWords words =
-  let singleLengthWords = filter (\s -> length s == 9) words
-   in [ [first, second, third] | first <- singleLengthWords, second <- singleLengthWords, first /= second, getSubstring first 3 6 == getSubstring second 0 3, third <- singleLengthWords, second /= third, first /= third, getSubstring third 0 3 == getSubstring first 6 9, getSubstring third 3 6 == getSubstring second 6 9
-      ]
-
-generalWatskyWords :: [String] -> Int -> Int -> [[String]] -> [[String]]
-generalWatskyWords words sylLength numWords currentWords =
-  [candidate | word <- words, candidate <- extend words sylLength numWords [word]]
-
-extend :: [String] -> Int -> Int -> [String] -> [[String]]
-extend words sylLength numWords currentWords
-  | length currentWords == numWords = [currentWords]
+extend :: [String] -> Int -> Int -> [[String]] -> [[String]]
+extend words sylLength numWords candidates
+  | length (head candidates) == numWords = candidates
   | otherwise =
-      [currentWords ++ [word] | word <- words, word `notElem` currentWords, checkSyls sylLength numWords currentWords word]
+      let newCans = concatMap (addNext words sylLength numWords) candidates
+       in extend words sylLength numWords newCans
 
-checkSyls :: Int -> Int -> [String] -> String -> Bool
-checkSyls sylLength numWords currentWords word = True
+addNext :: [String] -> Int -> Int -> [String] -> [[String]]
+addNext words sylLength numWords currentWords
+  | length currentWords == numWords = [currentWords]
+  | otherwise = [currentWords ++ [word] | word <- words, word `notElem` currentWords, checkWord sylLength numWords currentWords word]
 
--- let singleLengthWords = filter (\s -> length s == sylLength * numWords) words
--- in [ [first, second, third] | first <- singleLengthWords, second <- singleLengthWords, first /= second, getSubstring first 3 6 == getSubstring second 0 3, third <- singleLengthWords, second /= third, first /= third, getSubstring third 0 3 == getSubstring first 6 9, getSubstring third 3 6 == getSubstring second 6 9
---    ]
+checkWord :: Int -> Int -> [String] -> String -> Bool
+checkWord sylLength numWords currentWords word =
+  let sylToCheck = length currentWords
+      getSyl = getSubstring (sylLength * sylToCheck) (sylLength * (sylToCheck + 1))
+      syls = map getSyl currentWords
+      newSyls = [getSubstring (sylLength * i) (sylLength * (i + 1)) word | i <- [0 .. (sylToCheck - 1)]]
+   in syls == newSyls
+
+getSubstring :: Int -> Int -> String -> String
+getSubstring start end str = take (end - start) (drop start str)
+
+
+--watskyWords :: [String] -> [[String]]
+--watskyWords words =
+--  let singleLengthWords = filter (\s -> length s == 9) words
+--   in [ [first, second, third] | first <- singleLengthWords, second <- singleLengthWords, first /= second, getSubstring 3 6 first == getSubstring 0 3 second, third <- singleLengthWords, second /= third, first /= third, getSubstring 0 3 third == getSubstring 6 9 first, getSubstring 3 6 third == getSubstring 6 9 second
+--      ]
